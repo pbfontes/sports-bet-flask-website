dc1e99bee5a9ba35a1e688794e7cb52b4727cc58
Paulo Fontes <paulobatistaft@gmail.com>
2022-07-12 01:32:27 -0300

    testando sem o readme com mongo

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from flask import Flask
-#from DBconfig import collection
+from DBconfig import collection
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    # x = collection.find_one()
-    # x1 = x["name"]
-    # x2 = x["address"]
-    # return f"<h1>{x1}, {x2}</h1>"
-    return "teste"
+    x = collection.find_one()
+    x1 = x["name"]
+    x2 = x["address"]
+    return f"<h1>{x1}, {x2}</h1>"
+    # return "teste"
 
 
 if __name__ == '__main__':
